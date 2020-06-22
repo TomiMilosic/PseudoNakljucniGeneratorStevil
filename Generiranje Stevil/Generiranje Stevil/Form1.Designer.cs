d6d89af68492db1ca1a7cbc88e04fc3117a7c471
TomiMilosic <tomi.milosic@gmail.com>
2020-06-22 23:15:56 +0200

    Initial commit

---
+++ Generiranje Stevil/Generiranje Stevil/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Generiranje_Stevil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.Sinrb = new System.Windows.Forms.RadioButton();
+            this.Cosrb = new System.Windows.Forms.RadioButton();
+            this.SteviloX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.s10rb = new System.Windows.Forms.RadioButton();
+            this.s100rb = new System.Windows.Forms.RadioButton();
+            this.s1000rb = new System.Windows.Forms.RadioButton();
+            this.s10000rb = new System.Windows.Forms.RadioButton();
+            this.PoljubnoGeneriranjetxb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.GenerirajBtn = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Sinrb
+            // 
+            this.Sinrb.AutoSize = true;
+            this.Sinrb.Location = new System.Drawing.Point(18, 19);
+            this.Sinrb.Name = "Sinrb";
+            this.Sinrb.Size = new System.Drawing.Size(40, 17);
+            this.Sinrb.TabIndex = 0;
+            this.Sinrb.TabStop = true;
+            this.Sinrb.Text = "Sin";
+            this.Sinrb.UseVisualStyleBackColor = true;
+            // 
+            // Cosrb
+            // 
+            this.Cosrb.AutoSize = true;
+            this.Cosrb.Location = new System.Drawing.Point(76, 19);
+            this.Cosrb.Name = "Cosrb";
+            this.Cosrb.Size = new System.Drawing.Size(43, 17);
+            this.Cosrb.TabIndex = 1;
+            this.Cosrb.TabStop = true;
+            this.Cosrb.Text = "Cos";
+            this.Cosrb.UseVisualStyleBackColor = true;
+            // 
+            // SteviloX
+            // 
+            this.SteviloX.Location = new System.Drawing.Point(685, 85);
+            this.SteviloX.Name = "SteviloX";
+            this.SteviloX.Size = new System.Drawing.Size(109, 20);
+            this.SteviloX.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(662, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "X:";
+            // 
+            // s10rb
+            // 
+            this.s10rb.AutoSize = true;
+            this.s10rb.Location = new System.Drawing.Point(15, 19);
+            this.s10rb.Name = "s10rb";
+            this.s10rb.Size = new System.Drawing.Size(37, 17);
+            this.s10rb.TabIndex = 5;
+            this.s10rb.TabStop = true;
+            this.s10rb.Text = "10";
+            this.s10rb.UseVisualStyleBackColor = true;
+            // 
+            // s100rb
+            // 
+            this.s100rb.AutoSize = true;
+            this.s100rb.Location = new System.Drawing.Point(15, 54);
+            this.s100rb.Name = "s100rb";
+            this.s100rb.Size = new System.Drawing.Size(43, 17);
+            this.s100rb.TabIndex = 6;
+            this.s100rb.TabStop = true;
+            this.s100rb.Text = "100";
+            this.s100rb.UseVisualStyleBackColor = true;
+            // 
+            // s1000rb
+            // 
+            this.s1000rb.AutoSize = true;
+            this.s1000rb.Location = new System.Drawing.Point(70, 19);
+            this.s1000rb.Name = "s1000rb";
+            this.s1000rb.Size = new System.Drawing.Size(49, 17);
+            this.s1000rb.TabIndex = 7;
+            this.s1000rb.TabStop = true;
+            this.s1000rb.Text = "1000";
+            this.s1000rb.UseVisualStyleBackColor = true;
+            // 
+            // s10000rb
+            // 
+            this.s10000rb.AutoSize = true;
+            this.s10000rb.Location = new System.Drawing.Point(70, 54);
+            this.s10000rb.Name = "s10000rb";
+            this.s10000rb.Size = new System.Drawing.Size(55, 17);
+            this.s10000rb.TabIndex = 8;
+            this.s10000rb.TabStop = true;
+            this.s10000rb.Text = "10000";
+            this.s10000rb.UseVisualStyleBackColor = true;
+            // 
+            // PoljubnoGeneriranjetxb
+            // 
+            this.PoljubnoGeneriranjetxb.Location = new System.Drawing.Point(661, 228);
+            this.PoljubnoGeneriranjetxb.Name = "PoljubnoGeneriranjetxb";
+            this.PoljubnoGeneriranjetxb.Size = new System.Drawing.Size(133, 20);
+            this.PoljubnoGeneriranjetxb.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(662, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Poljubno generiranje:";
+            // 
+            // GenerirajBtn
+            // 
+            this.GenerirajBtn.Location = new System.Drawing.Point(661, 269);
+            this.GenerirajBtn.Name = "GenerirajBtn";
+            this.GenerirajBtn.Size = new System.Drawing.Size(102, 23);
+            this.GenerirajBtn.TabIndex = 12;
+            this.GenerirajBtn.Text = "Generiraj";
+            this.GenerirajBtn.UseVisualStyleBackColor = true;
+            this.GenerirajBtn.Click += new System.EventHandler(this.GenerirajBtn_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 48);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(603, 334);
+            this.chart1.TabIndex = 13;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Cosrb);
+            this.groupBox1.Controls.Add(this.Sinrb);
+            this.groupBox1.Location = new System.Drawing.Point(661, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(133, 43);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Izberi: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.s1000rb);
+            this.groupBox2.Controls.Add(this.s10rb);
+            this.groupBox2.Controls.Add(this.s100rb);
+            this.groupBox2.Controls.Add(this.s10000rb);
+            this.groupBox2.Location = new System.Drawing.Point(661, 125);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(133, 84);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Generiranje naslednjih: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(838, 405);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.GenerirajBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.PoljubnoGeneriranjetxb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SteviloX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Sinrb;
+        private System.Windows.Forms.RadioButton Cosrb;
+        private System.Windows.Forms.TextBox SteviloX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton s10rb;
+        private System.Windows.Forms.RadioButton s100rb;
+        private System.Windows.Forms.RadioButton s1000rb;
+        private System.Windows.Forms.RadioButton s10000rb;
+        private System.Windows.Forms.TextBox PoljubnoGeneriranjetxb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button GenerirajBtn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Generiranje Stevil/Generiranje Stevil/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Generiranje_Stevil
+{
+    public partial class Form1 : Form
+    {
+        int seme = 0;
+        List<double> stevila = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void GenerirajBtn_Click(object sender, EventArgs e)
+        {
+
+            double X = 0;
+            double.TryParse(SteviloX.Text, out X);
+
+            if (Sinrb.Checked)
+            {
+                if (s10rb.Checked) GenerirajSin(10, X);
+                if (s100rb.Checked) GenerirajSin(100, X);
+                if (s1000rb.Checked) GenerirajSin(1000, X);
+                if (s10000rb.Checked) GenerirajSin(10000, X);
+                if (PoljubnoGeneriranjetxb.Text != "" || PoljubnoGeneriranjetxb.Text != null)
+                {
+                    int stevilo = 0;
+                    if (int.TryParse(PoljubnoGeneriranjetxb.Text, out stevilo))
+                    {
+                        GenerirajSin(stevilo, X);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Napaka!");
+                    }
+                }
+
+            }
+
+            if (Cosrb.Checked)
+            {
+                if (s10rb.Checked) GenerirajCos(10, X);
+                if (s100rb.Checked) GenerirajCos(100, X);
+                if (s1000rb.Checked) GenerirajCos(1000, X);
+                if (s10000rb.Checked) GenerirajCos(10000, X);
+                if (PoljubnoGeneriranjetxb.Text != "" || PoljubnoGeneriranjetxb.Text != null)
+                {
+                    int stevilo = 0;
+                    if (int.TryParse(PoljubnoGeneriranjetxb.Text, out stevilo))
+                    {
+                        GenerirajCos(stevilo, X);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Napaka!");
+                    }
+                }
+
+            }
+
+
+
+        }
+
+
+
+        public void GenerirajSin(int steviloPonovitev, double x)
+        {
+            for (int i = 0; i < steviloPonovitev; i++)
+            {
+                seme = (int)(DateTime.Now.Ticks % 1000);
+                double y = Math.Round(seme * Math.Sin(x - (Math.PI / 3)), 2);
+                stevila.Add(y);
+                x = y;
+            }
+
+           
+
+            using (var pisi = new StreamWriter(@"seznamStevil.txt"))
+            {
+                foreach (var item in stevila)
+                {
+                    pisi.Write(item + ",");
+                }
+
+            }
+
+            StPojavitev(stevila);
+
+        }
+
+
+        public void GenerirajCos(int steviloPonovitev, double x)
+        {
+            for (int i = 0; i < steviloPonovitev; i++)
+            {
+                seme =(int)(DateTime.Now.Ticks % 1000);
+                double y = Math.Round(seme * Math.Cos(x + (Math.PI / 6)), 2);
+                stevila.Add(y);
+                x = y;
+            }
+
+
+            using (var pisi = new StreamWriter(@"seznamStevil.txt"))
+            {
+                foreach (var item in stevila)
+                {
+                    pisi.Write(item + ",");
+                }
+
+            }
+            StPojavitev(stevila);
+
+        }
+
+
+        public void StPojavitev(List<double> stevila)
+        {
+
+            chart1.Series.Clear();
+
+            var edinstvena = stevila.GroupBy(x => x);
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Column;
+            
+            foreach (var item in edinstvena)
+            {
+                series.Points.AddXY(item.Key,item.Count());
+            }
+
+            chart1.Series.Add(series);
+
+        }
+    }
+}
